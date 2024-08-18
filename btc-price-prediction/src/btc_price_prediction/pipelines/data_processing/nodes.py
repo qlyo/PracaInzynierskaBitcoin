cfef1effea3eba6f9cd6036cb098ea7d6b5b5817
wegris <gawelb2k@gmail.com>
2024-08-18 15:07:05 +0200

    Adding download raw btc data option
    Przy uruchamianiu mozna podac parametr forcedownload na true, zeby wymusic pobieranie nowych danych

--- btc-price-prediction/src/btc_price_prediction/pipelines/data_processing/nodes.py
+++ btc-price-prediction/src/btc_price_prediction/pipelines/data_processing/nodes.py
@@ -6,9 +6,42 @@ import pandas as pd
 import datetime as dt
 from sklearn.preprocessing import MinMaxScaler
 import yfinance as yf
+from kedro.framework.session import KedroSession
+from pathlib import Path
+
+
+def download_data(crypto_currency: str, against_currency: str, start_date: dt.datetime, end_date: str,
+                  force_download: bool) -> pd.DataFrame:
+    project_path = Path.cwd()
+
+    # Inicjalizacja sesji Kedro i katalogu danych
+    with KedroSession.create(project_path=project_path) as session:
+        context = session.load_context()
+        catalog = context.catalog
+
+        if end_date == "now":
+            end_date = dt.datetime.now()
+        print('*'*20)
+        if not force_download:
+            try:
+                btc_raw_data = catalog.load("btc_raw_data")
+                print('Data loaded from catalog')
+            except:
+                print('Data not found in catalog, downloading data')
+                ticker = f'{crypto_currency}-{against_currency}'
+                btc_raw_data = yf.download(ticker, start=start_date, end=end_date)
+                catalog.save("btc_raw_data", btc_raw_data)
+        else:
+            print("Force download data")
+            ticker = f'{crypto_currency}-{against_currency}'
+            btc_raw_data = yf.download(ticker, start=start_date, end=end_date)
+            catalog.save("btc_raw_data", btc_raw_data)
+        print('*' * 20)
+    return btc_raw_data
+
 
 def preprocess_btc_raw(btc_raw: pd.DataFrame) -> pd.DataFrame:
-    #pobieranie z yf
+    # pobieranie z yf
     crypto_currency = 'BTC'
     against_currency = 'USD'
     start = dt.datetime(2016, 1, 1)
@@ -19,9 +52,9 @@ def preprocess_btc_raw(btc_raw: pd.DataFrame) -> pd.DataFrame:
     btc_preprocessed_data = yf.download(ticker, start=start, end=end)
 
     #################################################################################
-    #używanie z kegla danych
+    # używanie z kegla danych
     # Dropping columns
-    #btc_preprocessed_data = btc_raw.drop(columns=['next_day_close'])
+    # btc_preprocessed_data = btc_raw.drop(columns=['next_day_close'])
     #################################################################################
     # Scaling values
     scaler = MinMaxScaler(feature_range=(0, 1))
--- btc-price-prediction/src/btc_price_prediction/pipelines/data_processing/pipeline.py
+++ btc-price-prediction/src/btc_price_prediction/pipelines/data_processing/pipeline.py
@@ -5,11 +5,18 @@ generated using Kedro 0.19.6
 
 from kedro.pipeline import Pipeline, pipeline, node
 
-from .nodes import preprocess_btc_raw
+from .nodes import preprocess_btc_raw, download_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
+        node(
+            func=download_data,
+            inputs=["params:crypto_currency", "params:against_currency", "params:start_date", "params:end_date",
+                    "params:force_download"],
+            outputs="btc_raw_data",
+            name="download_data_node"
+        ),
         node(
             func=preprocess_btc_raw,
             inputs="btc_raw_data",
