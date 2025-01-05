f93661dd8fa93a27536d1656aa4e6e98d348afed
wegris <gawelb2k@gmail.com>
2025-01-05 22:13:31 +0100

    Created 2 blank pipelines

---
+++ btcpriceprediction-features/src/btcpriceprediction_features/pipelines/data_processing/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.19.10
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
---
+++ btcpriceprediction-features/src/btcpriceprediction_features/pipelines/data_science/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'data_science'
+generated using Kedro 0.19.10
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
